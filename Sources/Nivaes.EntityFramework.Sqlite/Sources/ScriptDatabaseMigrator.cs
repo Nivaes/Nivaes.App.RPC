2d09bf54d9b6efb05e59628466fb819ccbc2432b
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-08-19 19:06:06 +0200

    Define AotCompatible

--- Sources/Nivaes.EntityFramework.Sqlite/Sources/ScriptDatabaseMigrator.cs
+++ Sources/Nivaes.EntityFramework.Sqlite/Sources/ScriptDatabaseMigrator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.Data.Sqlite;
 
 namespace Nivaes.EntityFrameworkCore.Sqlite;
 
