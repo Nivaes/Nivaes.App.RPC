a69b68290a873526ee5f3524ecf9af080acb2411
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-08-22 20:30:02 +0200

    Serialize

--- Samples/Nivaes.App.Rpc.Sample.Tests/Tests/Serializer/SerializeTest.cs
+++ Samples/Nivaes.App.Rpc.Sample.Tests/Tests/Serializer/SerializeTest.cs
@@ -10,7 +10,7 @@ namespace Nivaes.App.Rpc.Test.Serializer
     public class SerializeTest
     {
         [Fact]
-        public void SerializerModel()
+        public void SerializerDataModel()
         {
             var contact = ContactGenerator.GenerateContact();
 
@@ -28,6 +28,79 @@ namespace Nivaes.App.Rpc.Test.Serializer
 
             var bin = MemoryPackSerializer.Serialize(user);
             var userCopy = MemoryPackSerializer.Deserialize<UserDataModel>(bin);
+
+            userCopy.ShouldNotBeNull();
+            userCopy.Id.ShouldBe(user.Id);
+            userCopy.IdUser.ShouldBe(user.IdUser);
+
+            userCopy.IdUser.ShouldBe(user.IdUser);
+            userCopy.Identification.ShouldBe(user.Identification);
+            userCopy.Name.ShouldBe(user.Name);
+            userCopy.GivenName.ShouldBe(user.GivenName);
+            userCopy.FamilyName.ShouldBe(user.FamilyName);
+            userCopy.Email.ShouldBe(user.Email);
+            userCopy.IdUser.ShouldBe(user.IdUser);
+            userCopy.PhoneNumber.ShouldBe(user.PhoneNumber);
+            userCopy.TimeStamp.ShouldBe(user.TimeStamp);
+        }
+
+        [Fact]
+        public void SerializerDataModelOverwrite()
+        {
+            var contact = ContactGenerator.GenerateContact();
+
+            var user = new UserDataModel
+            {
+                IdUser = Guid.NewGuid(),
+                Identification = $"ID00000",
+                Name = contact.SortName,
+                GivenName = contact.GivenName,
+                FamilyName = contact.FamilyName,
+                Email = contact.Email,
+                PhoneNumber = contact.TelephoneNumber,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            UserDataModel? userCopy = null;
+            var bin = MemoryPackSerializer.Serialize(user);
+            var i = MemoryPackSerializer.Deserialize(bin, ref userCopy);
+
+            userCopy.ShouldNotBeNull();
+            userCopy.Id.ShouldBe(user.Id);
+            userCopy.IdUser.ShouldBe(user.IdUser);
+
+            userCopy.IdUser.ShouldBe(user.IdUser);
+            userCopy.Identification.ShouldBe(user.Identification);
+            userCopy.Name.ShouldBe(user.Name);
+            userCopy.GivenName.ShouldBe(user.GivenName);
+            userCopy.FamilyName.ShouldBe(user.FamilyName);
+            userCopy.Email.ShouldBe(user.Email);
+            userCopy.IdUser.ShouldBe(user.IdUser);
+            userCopy.PhoneNumber.ShouldBe(user.PhoneNumber);
+            userCopy.TimeStamp.ShouldBe(user.TimeStamp);
+        }
+
+        [Fact]
+        public async Task SerializerDataModelAsync()
+        {
+            var contact = ContactGenerator.GenerateContact();
+
+            var user = new UserDataModel
+            {
+                IdUser = Guid.NewGuid(),
+                Identification = $"ID00000",
+                Name = contact.SortName,
+                GivenName = contact.GivenName,
+                FamilyName = contact.FamilyName,
+                Email = contact.Email,
+                PhoneNumber = contact.TelephoneNumber,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            MemoryStream ms = new MemoryStream();
+            await MemoryPackSerializer.SerializeAsync(ms, user);
+            ms.Position = 0;
+            var userCopy = await MemoryPackSerializer.DeserializeAsync<UserDataModel>(ms);
 
             userCopy.ShouldNotBeNull();
             userCopy.Id.ShouldBe(user.Id);
